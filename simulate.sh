ada5cfdae945bf783dc1c09af785565edd34c238
mbmanu <mbmanu74@gmail.com>
2022-09-19 21:45:16 +0530

    modified simulation files

--- simulate.sh
+++ simulate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-iverilog -Wall -g2012 -o run testbench.sv alu.v cond.v ram.v rvcore.v && vvp run +firmware=/d/Projects/MajorProject/test.bin
+iverilog -Wall -g2012 -o run testbench.sv alu.v cond.v ram.v rvcore.v && vvp run +firmware=/d/Major_Project/MajorProject/test.bin > output.txt
